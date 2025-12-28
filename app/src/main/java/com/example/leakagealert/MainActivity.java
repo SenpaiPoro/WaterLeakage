5b73d9c7b797b76407041a3dbbae947242d03a51
Marlon Jr Mullet <marlonmullet10@gmail.com>
2025-12-29 03:39:31 +0800

    hello

--- app/src/main/java/com/example/leakagealert/MainActivity.java
+++ app/src/main/java/com/example/leakagealert/MainActivity.java
@@ -1,25 +1,59 @@
 package com.example.leakagealert;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.leakagealert.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvDistance, tvPercentage, tvLiters;
+    DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        tvDistance = findViewById(R.id.tvDistance);
+        tvPercentage = findViewById(R.id.tvPercentage);
+        tvLiters = findViewById(R.id.tvLiters);
+
+        dbRef = FirebaseDatabase.getInstance()
+                .getReference("WaterTank")
+                .child("Data");
+
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                if (snapshot.exists()) {
+                    Double distance = snapshot.child("distance_cm").getValue(Double.class);
+                    Double percentage = snapshot.child("percentage").getValue(Double.class);
+                    Double liters = snapshot.child("liters").getValue(Double.class);
+
+                    if (distance != null)
+                        tvDistance.setText("Distance: " + distance + " cm");
+
+                    if (percentage != null)
+                        tvPercentage.setText("Tank Level: " + percentage + " %");
+
+                    if (liters != null)
+                        tvLiters.setText("Water Lost: " + liters + " L");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                tvDistance.setText("Firebase error");
+            }
         });
     }
-
 }
